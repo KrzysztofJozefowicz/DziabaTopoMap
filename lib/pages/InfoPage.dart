95a844f9c573871a95858092209dc35ee389ca2f
Krzysztof Jozefowicz <KrzysztofJozefowicz@users.noreply.github.com>
2020-07-23 16:19:16 +0200

    customIcons import update

--- lib/pages/InfoPage.dart
+++ lib/pages/InfoPage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../widgets/Drawer.dart';
-import '../widgets/GitHubIcon.dart';
+import '../Helpers/CustomIcons.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class InfoPage extends StatefulWidget {
