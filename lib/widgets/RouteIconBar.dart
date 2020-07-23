b99a423c79298e658bf3adb757820e6a4601bf06
Krzysztof Jozefowicz <KrzysztofJozefowicz@users.noreply.github.com>
2020-07-23 16:20:49 +0200

    - changed icons to red - change zoom in icon - added drop down menu when opening topo in external browser - now list of all possible urls are shown

--- lib/widgets/RouteIconBar.dart
+++ lib/widgets/RouteIconBar.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../dataProvider/RockLoader.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -6,6 +7,8 @@ import '../states/AppState.dart';
 import 'package:maps_launcher/maps_launcher.dart';
 import '../Helpers/Helpers.dart';
 import 'package:latlong/latlong.dart';
+import '../Helpers/CustomIcons.dart';
+import 'dart:developer';
 
 class buildIconBar extends StatefulWidget {
   final Rock _rockItem;
@@ -16,28 +19,25 @@ class buildIconBar extends StatefulWidget {
   @override
   State<StatefulWidget> createState() =>
       //_FavoriteButtonState();
-  buildIconBarState(this._rockItem, this._mapController);
+      buildIconBarState(this._rockItem, this._mapController);
 }
 
 class buildIconBarState extends State<buildIconBar> {
   final Rock _rockItem;
   final MapController _mapController;
-  final double iconSize=30.0;
+  final double iconSize = 30.0;
 
   buildIconBarState(this._rockItem, this._mapController);
 
   @override
   Widget build(BuildContext context) {
     var myState = Provider.of<appState>(context, listen: true);
-    return Row(
-
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          InkWell(
-              child: Container(
-                child: _favoriteIcons[myState.isInFavorites(_rockItem.id)],
-              ),
-              onTap: () => setState(() {
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+      InkWell(
+          child: Container(
+            child: _favoriteIcons[myState.isInFavorites(_rockItem.id)],
+          ),
+          onTap: () => setState(() {
                 if (myState.isInFavorites(_rockItem.id)) {
                   myState.removeFromFavorites(_rockItem.id);
                 } else {
@@ -45,36 +45,73 @@ class buildIconBarState extends State<buildIconBar> {
                 }
                 myState.filterContent["favorites"] = myState.favorites;
               })),
-          InkWell(
-              child: Container(
-                  child: Icon(Icons.navigation, color: Colors.blue, size: iconSize,)),
-              onTap: () => setState(() {
-                MapsLauncher.launchCoordinates(
-                    double.parse(_rockItem.lat), double.parse(_rockItem.lng ));
+      InkWell(
+          child: Container(
+              child: Icon(
+            Icons.navigation,
+            color: Colors.red,
+            size: iconSize,
+          )),
+          onTap: () => setState(() {
+                MapsLauncher.launchCoordinates(double.parse(_rockItem.lat), double.parse(_rockItem.lng));
               })),
-          InkWell(
-              child: Container(
-                  child: Icon(Icons.info, color: Colors.blue, size: iconSize)),
-              onTap: () => setState(() {
-                //FIXME: hardoced infoPage to topo.portalgorski.pl !!!
-                launchURL(_rockItem.infoPage[0]["topo.portalgorski.pl"]);
-              })),
-
-          InkWell(
-              child: Container(
-                child: Icon(Icons.zoom_out_map, color: Colors.red, size: iconSize),
-              ),
-              onTap: () => setState(() {
+      InkWell(
+          child: Container(
+              child: DropDownOpenInfoPage(_rockItem.infoPage)
+              )),
+      InkWell(
+          child: Container(
+            child: Icon(Icons.zoom_in, color: Colors.red, size: iconSize),
+          ),
+          onTap: () => setState(() {
                 // TODO: move previous map possition and zoom to state
                 LatLng latlng = LatLng(double.parse(_rockItem.lat), double.parse(_rockItem.lng));
                 double zoom = 15.0; //the zoom you want
                 _mapController.move(latlng, zoom);
               }))
-        ]);
+    ]);
   }
 
+  PopupMenuButton<String> DropDownOpenInfoPage(List<InfoPage> infoPage) {
 
 
+    return PopupMenuButton<String>(
+      color: Theme.of(context).primaryColor,
+      icon: Icon(
+        Icons.info,
+        color: Colors.red,
+        size: iconSize,
+
+      ),
+      offset: Offset(105,200),
+      itemBuilder: (context) => GeneratePopupMenuItem(infoPage),
+
+      onSelected: (value) {
+        launchURL(value);
+      },
+    );
+
+  }
+  List<PopupMenuItem<String>> GeneratePopupMenuItem(List<InfoPage> infoPage)
+  {
+    List<PopupMenuItem<String>>myList = new List();
+    for (var element in infoPage)
+      {
+        myList.add(
+            new PopupMenuItem<String>(
+                value: element.url,
+
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget> [
+                    Icon(GlobIcon.globe, color: Colors.black,),
+                    SizedBox(width: 10,),
+                    Text(element.displayName, style: TextStyle(color: Colors.black))],
+                )
+            ));
+      }
+    return myList;
+  }
   final Map<bool, Widget> _favoriteIcons = {
     false: Icon(
       Icons.favorite,
